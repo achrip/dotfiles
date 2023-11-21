5b3f990d5f5ac0862d1916c1e3a3277bc8b22726
Ashraf Alif Adillah <ashraf.alif@gmail.com>
2023-11-21 21:19:07 +0700

    feat: add goyo, zen-mode, and vim-jukit
    `goyo` and `zen-mode` is essentially the same; to help focus and whatnot.
    
    `vim-jukit` is plugin for Jupyter Notebook

--- .config/nvim/lua/achrip/plugins.lua
+++ .config/nvim/lua/achrip/plugins.lua
@@ -1,41 +1,49 @@
 return {
-  'wbthomason/packer.nvim', 
-  { 
+  'wbthomason/packer.nvim',
+  {
     'nvim-telescope/telescope.nvim',
     tag = '0.1.1',
-    dependencies = { 'nvim-lua/plenary.nvim'} 
+    dependencies = { 'nvim-lua/plenary.nvim'}
   },
 
-  { 
-    'nvim-treesitter/nvim-treesitter', 
-    build = ':TSUpdate' 
-  }, 
+  {
+    'nvim-treesitter/nvim-treesitter',
+    build = ':TSUpdate'
+  },
 
-  'nvim-treesitter/playground', 
-  'mbbill/undotree', 
-  'tpope/vim-fugitive', 
+  'nvim-treesitter/playground',
+  'mbbill/undotree',
+  'tpope/vim-fugitive',
 
   {
-    'VonHeikemen/lsp-zero.nvim', 
+    'VonHeikemen/lsp-zero.nvim',
     branch = 'v3.x',
     dependencies = {
-    
-      'williamboman/mason.nvim', 
+
+      'williamboman/mason.nvim',
       'williamboman/mason-lspconfig.nvim',
-      'neovim/nvim-lspconfig', 
+      'neovim/nvim-lspconfig',
       'neovim/nvim-lspconfig',
       'hrsh7th/cmp-nvim-lsp',
       'hrsh7th/nvim-cmp',
       'L3MON4D3/Luasnip'
-  
+
       }
   },
 
-    'rebelot/kanagawa.nvim', 
+    'rebelot/kanagawa.nvim',
     'snelling-a/base16.nvim',
   {
-    'folke/tokyonight.nvim', 
-    lazy = true, 
+    'folke/tokyonight.nvim',
+    lazy = true,
     opts = { style = 'moon' },
   },
+  {
+    "catppuccin/nvim",
+    name = "catppuccin",
+    priority = 1000
+  },
+  { 'junegunn/goyo.vim' },
+  { 'folke/zen-mode.nvim' },
+  { 'luk400/vim-jukit' },
 }
