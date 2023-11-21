43177882fe620957263e6337b784df1073329ac3
Ashraf Alif Adillah <ashraf.alif@gmail.com>
2023-11-21 21:17:43 +0700

    fix: change colorscheme to catppuccin and make it transparent

--- .config/nvim/after/plugin/colors.lua
+++ .config/nvim/after/plugin/colors.lua
@@ -1,3 +1,14 @@
-vim.cmd.colorscheme("base16-tomorrow-night-eighties")
-vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
-vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
+require("catppuccin").setup({
+  transparent_background = true,
+})
+
+function ColorMyPencils(color)
+  color = color or "catppuccin-mocha"
+  vim.cmd.colorscheme(color)
+
+  vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
+  vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
+  vim.api.nvim_set_hl(0, "StatusLine", { ctermfg = 1} )
+end
+
+ColorMyPencils()
