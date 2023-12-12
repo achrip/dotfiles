9b45cca5230dc0093f332a501a52a91ec334c756
Ashraf Alif Adillah <ashraf.alif@gmail.com>
2023-12-12 20:35:11 +0700

    feat: add neorg plugin
    this commit also removes `goyo.vim` and `vim-jukit`

--- .config/nvim/lua/achrip/plugins.lua
+++ .config/nvim/lua/achrip/plugins.lua
@@ -43,7 +43,26 @@ return {
     name = "catppuccin",
     priority = 1000
   },
-  { 'junegunn/goyo.vim' },
   { 'folke/zen-mode.nvim' },
-  { 'luk400/vim-jukit' },
+  {
+    "nvim-neorg/neorg",
+    build = ":Neorg sync-parsers",
+    dependencies = { "nvim-lua/plenary.nvim" },
+    config = function()
+      require("neorg").setup {
+        load = {
+          ["core.defaults"] = {}, -- Loads default behaviour
+          ["core.concealer"] = {}, -- Adds pretty icons to your documents
+          ["core.export"] = {},
+          ["core.dirman"] = { -- Manages Neorg workspaces
+          config = {
+            workspaces = {
+              notes = "~/notes",
+            },
+          },
+        },
+      },
+    }
+  end,
+},
 }
