4a36ab57a6d20095e825a1dabad5e4f771b4d7bb
Ashraf Alif Adillah <ashraf.alif@gmail.com>
2024-05-17 13:50:30 +0700

    fix: remove plugins

--- .config/nvim/lua/achrip/plugins.lua
+++ .config/nvim/lua/achrip/plugins.lua
@@ -1,25 +1,20 @@
 return {
-  'wbthomason/packer.nvim',
   {
     'nvim-telescope/telescope.nvim',
     tag = '0.1.4',
     dependencies = { 'nvim-lua/plenary.nvim'}
   },
-
   {
     'nvim-treesitter/nvim-treesitter',
     build = ':TSUpdate'
   },
-
   'nvim-treesitter/playground',
   'mbbill/undotree',
   'tpope/vim-fugitive',
-
   {
     'VonHeikemen/lsp-zero.nvim',
     branch = 'v3.x',
     dependencies = {
-
       'williamboman/mason.nvim',
       'williamboman/mason-lspconfig.nvim',
       'neovim/nvim-lspconfig',
@@ -27,19 +22,9 @@ return {
       'hrsh7th/cmp-nvim-lsp',
       'hrsh7th/nvim-cmp',
       'L3MON4D3/Luasnip'
-
       }
   },
-
-    'rebelot/kanagawa.nvim',
     'rithikasilva/sequoia-monochrome.nvim',
-    'kdheepak/monochrome.nvim',
-    'cranberry-clockworks/coal.nvim',
-  {
-    'folke/tokyonight.nvim',
-    lazy = true,
-    opts = { style = 'moon' },
-  },
   {
     "catppuccin/nvim",
     name = "catppuccin",
