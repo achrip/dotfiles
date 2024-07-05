033a49a518ab391429f3d8b10dc83722d35f60fa
Ashraf Alif Adillah <ashraf.alif@gmail.com>
2024-07-05 15:21:35 +0700

    chore: cleanup

--- .config/nvim/after/plugin/treesitter.lua
+++ .config/nvim/after/plugin/treesitter.lua
@@ -1,6 +1,6 @@
 require'nvim-treesitter.configs'.setup {
   -- A list of parser names, or "all" (the five listed parsers should always be installed)
-  ensure_installed = { "c", "lua", "vim", "vimdoc", "query"},
+  ensure_installed = "all",
 
   -- Install parsers synchronously (only applied to `ensure_installed`)
   sync_install = false,
@@ -18,5 +18,25 @@ require'nvim-treesitter.configs'.setup {
     -- Instead of true it can also be a list of languages
     additional_vim_regex_highlighting = false,
   },
-
 }
+
+local parser_config = require "nvim-treesitter.parsers".get_parser_configs()
+parser_config.blade = {
+  install_info = {
+    url = 'https://github.com/EmranMR/tree-sitter-blade',
+    files = {
+      'src/parser.c',
+      -- 'src/scanner.cc',
+    },
+    branch = 'main',
+    generate_requires_npm = true,
+    requires_generate_from_grammar = true,
+  },
+  filetype = 'blade',
+}
+
+vim.filetype.add({
+  pattern = {
+    ['.*%.blade%.php'] = 'blade',
+  }
+})
--- .config/nvim/lua/achrip/plugins.lua
+++ .config/nvim/lua/achrip/plugins.lua
@@ -1,35 +1,38 @@
 return {
+  -- Colorscheme
+--  {
+--    'rithikasilva/sequoia-monochrome.nvim',
+--    name = 'sequoia monochrome'
+--  }, 
+  {
+    'Hiroya-W/sequoia-moonlight.nvim', 
+    name = 'sequoia moonlight'
+  },
+  {
+    'catppuccin/nvim',
+    name = 'catppuccin',
+    priority = 1000
+  },
+  {
+    'rose-pine/neovim', 
+    name = 'rose-pine'
+  },
+
+  -- Other
   {
     'nvim-telescope/telescope.nvim',
     tag = '0.1.4',
     dependencies = { 'nvim-lua/plenary.nvim'}
   },
+  { 'mbbill/undotree' },
+  { 'tpope/vim-fugitive' } ,
+  { 'folke/zen-mode.nvim' },
+
+  -- LSP and highlighting 
   {
     'nvim-treesitter/nvim-treesitter',
     build = ':TSUpdate', 
-    config = function(_, opts)
-      local parser_config = require('nvim-treesitter.parsers').get_parser_configs()
-
-      parser_config.blade = {
-        install_info = {
-          url = 'https://github.com/EmranMR/tree-sitter-blade',
-          files = {
-            'src/parser.c',
-            -- 'src/scanner.cc',
-          },
-          branch = 'main',
-          generate_requires_npm = true,
-          requires_generate_from_grammar = true,
-        },
-        filetype = 'blade',
-      }
-
-      require('nvim-treesitter.configs').setup(opts)
-    end
   },
-  'nvim-treesitter/playground',
-  'mbbill/undotree',
-  'tpope/vim-fugitive',
   {
     'VonHeikemen/lsp-zero.nvim',
     branch = 'v3.x',
@@ -43,24 +46,5 @@ return {
       'L3MON4D3/Luasnip'
       }
   },
---  {
---    'rithikasilva/sequoia-monochrome.nvim',
---    name = "sequoia monochrome"
---  }, 
-  {
-    'Hiroya-W/sequoia-moonlight.nvim', 
-    name = 'sequoia moonlight'
-  },
-  {
-    "catppuccin/nvim",
-    name = "catppuccin",
-    priority = 1000
-  },
-  { 'folke/zen-mode.nvim' },
-  {
-    'quarto-dev/quarto-nvim', 
-    dependencies = { 
-      'jmbuhr/otter.nvim', 
-    }
-  }
+  { 'nvim-treesitter/playground' },
 }
